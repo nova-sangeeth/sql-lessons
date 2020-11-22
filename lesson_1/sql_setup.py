e42c7d9e379b3ca217c9cde23f973a188af9d51c
nova-sangeeth <novasangeeth@outlook.com>
2020-11-22 21:13:59 +0530

    made changes to the file and restructured it according to the notebook.

--- lesson_1/sql_setup.py
+++ lesson_1/sql_setup.py
@@ -24,26 +24,25 @@ def read_sql(sql_query):
     return pd.read_sql(sql_query, connection)
 
 def list_tables():
-  return read_sql("SELECT * FROM sqlite_master WHERE type='table' and name not like 'sqlite_%' ORDER BY name;")
+    return read_sql("SELECT * FROM sqlite_master WHERE type='table' and name not like 'sqlite_%' ORDER BY name;")
+
 
 query1 = '''
-CREATE TABLE regions (
-    region_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
-    region_name varchar(20) ,
-    region_code varchar(20)
-)
-'''
-query2 = '''
 CREATE TABLE countries (
-    country_id text PRIMARY KEY  NOT NULL,
-    region_id INTEGER NOT NULL,
-    country_growthRate decimal(5,2) DEFAULT 0,  /* Decimal type conversion */
+    country_id INTEGER PRIMARY KEY NOT NULL, 
+    region_id INTEGER NO NULL, 
+    country_growthRate decimal(5,2) DEFAULT 0,
     FOREIGN KEY (region_id) REFERENCES
     regions (region_id) ON DELETE CASCADE ON UPDATE CASCADE
 )
 '''
-
 execute(queryString=query1)
+
+query2 = '''
+CREATE TABLE regions (
+    region_id INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, region_name varchar(20), region_code varchar(20)
+)
+'''
 execute(queryString=query2)
 
-
+list_tables()
